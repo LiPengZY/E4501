0c627a12d2ec7df9c4f98b0ac581ab6a39c08c31
dyy222dyy222 <yydai222@outlook.com>
2021-11-15 21:32:32 -0500

    Update

--- test_portfolio.py
+++ test_portfolio.py
@@ -4,3 +4,8 @@ def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
     assert p.cost() == 17648.0
+
+def test_buy_one_stock():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    assert p.cost() == 17648.0
